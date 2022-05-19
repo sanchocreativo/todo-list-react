ca9e3205eadc239660c020e1f9c48505b9fb00a3
sanchocreativo <sanchocreativo@gmail.com>
2022-05-19 17:25:45 -0300

    changes for localstorage

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import * as React from "react";
-import { render } from "react-dom";
+import {createRoot} from 'react-dom/client';
+
 import { Provider } from "react-redux";
 import store from "./store/store";
 import TodoList from "./components/TodoList";
 import TodoInput from "./components/TodoInput";
+import { debounce } from "debounce";
+import { saveState } from "./store/localStorage";
 
 import "./index.scss";
 
+store.subscribe(
+  debounce(() => {
+    saveState(store.getState());
+  }, 800)
+);
 function App() {
   return (
     <Provider store={store}>
@@ -19,5 +27,7 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+const rootElement = document.getElementById("root")!;
+const root = createRoot(rootElement);
+
+root.render(<App />);
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,14 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import todos from './todos';
-
+import { loadState } from './localStorage';
 const rootReducer = combineReducers({
   todos: todos.reducer,
 });
 
 const store = configureStore({
-  reducer: rootReducer,
+    devTools: true,
+    reducer: rootReducer,
+    preloadedState: loadState(),
 });
 
 export type AppDispatch = typeof store.dispatch;
